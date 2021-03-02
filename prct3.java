9e945733d7fa7370a681960be9a15efcbd9e7fcc
SwamiDevika <66199067+SwamiDevika@users.noreply.github.com>
2021-03-02 22:28:29 +0530

    Add files via upload

--- prct3.java
+++ prct3.java
@@ -6,20 +6,32 @@ public class prct3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan =new Scanner(System.in);
-		int n= scan.nextInt();
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
 		factor(n);
+		sumFactor(n);
+	}
+
+	private static void sumFactor(int n) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for (int i = 1; i <= n; ++i) {
+			if (n % i == 0) {
+				sum+=i;
+				System.out.println(sum);
+			}
+		}
 
 	}
 
 	private static void factor(int n) {
 		// TODO Auto-generated method stub
-		for(int i=1;i<=n;++i) {
-			if(n%i==0) {
+		for (int i = 1; i <= n; ++i) {
+			if (n % i == 0) {
 				System.out.println(i);
 			}
 		}
-			
+
 	}
 
 }
